d50cfb511047fb2193b4d8c8ef2b225c1808a185
lewis <lewis@mail.ybico.com.tw>
2023-01-19 13:32:04 +0800

    update

--- Trading_Systeam.py
+++ Trading_Systeam.py
@@ -9,6 +9,7 @@ import sys
 # 下單物件未完成測試
 # 實際測試
 # GUI閃退問題
+# 資料保存覆蓋問題
 
 class Trading_systeam():
     def __init__(self) -> None:
@@ -26,8 +27,14 @@ class Trading_systeam():
         # 取得交易標的(頻率不需要太頻繁 一個月一次即可)
         # ['btcdomusdt-f-d', 0.061946189785725636], ['xmrusdt-f-d', 0.055462536626203374], ['btcstusdt-f-d', 0.0]
 
+    def printfunc(self, *args):
+        out_str = ''
+        for i in args:
+            out_str += str(i)+" "
+            print(out_str)
+
     def main(self):
-        print("開始交易!!!")
+        self.printfunc("開始交易!!!")
         self.engine.Portfolio_online_register()
         symbol_name: list = self.engine.get_symbol_name()
 
@@ -35,7 +42,7 @@ class Trading_systeam():
         for each_symbol in symbol_name:
             Response = self.dataprovider_online.Binanceapp.client.futures_change_leverage(
                 symbol=each_symbol, leverage=20)
-            print(Response)
+            self.printfunc(Response)
 
         # 先將資料從DB撈取出來
         for name in symbol_name:
@@ -44,10 +51,10 @@ class Trading_systeam():
             self.symbol_map.update({name: original_df})
 
         last_min = None
-        print("資料讀取結束")
+        self.printfunc("資料讀取結束")
         # 透過迴圈回補資料
         while True:
-            print(datetime.now().minute)
+            self.printfunc(datetime.now().minute)
             if datetime.now().minute != last_min or last_min is None:
                 # if True:
                 # 需要個別去計算每個loop所耗費的時間
@@ -69,33 +76,45 @@ class Trading_systeam():
                     # 將資料注入
                     self.engine.register_data(strategy_name, trade_data)
 
-                print("開始進入回測")
+                self.printfunc("開始進入回測")
                 # 註冊完資料之後進入回測
                 pf = self.engine.Portfolio_online_start()
-                print("最後資料表*************************************")
+                self.printfunc("最後資料表*************************************")
                 last_status = pf.get_last_status()
-                print('目前交易狀態', last_status)
-                print("最後資料表*************************************")
+                self.printfunc('目前交易狀態', last_status)
+                self.printfunc("最後資料表*************************************")
 
                 current_size = self.dataprovider_online.Binanceapp.getfutures_account_positions()
-                print("目前binance交易所內的部位狀態:", current_size)
+                self.printfunc("目前binance交易所內的部位狀態:", current_size)
 
                 # >>比對目前binance 內的部位狀態 進行交易
                 order_finally = self.dataprovider_online.transformer.calculation_size(
                     last_status, current_size)
-                print("差異單", order_finally)
+                self.printfunc("差異單", order_finally)
                 if order_finally:
                     self.dataprovider_online.Binanceapp.execute_orders(
                         order_finally)
 
-                print("時間差", time.time() - begin_time)
+                self.printfunc("時間差", time.time() - begin_time)
                 last_min = datetime.now().minute
             else:
                 time.sleep(1)
 
             if self.dataprovider_online.Binanceapp.trade_count > 10:
-                print("緊急狀況處理-交易次數過多")
+                self.printfunc("緊急狀況處理-交易次數過多")
                 sys.exit()
+
+
+class GUI_Trading_systeam(Trading_systeam):
+    def __init__(self, GUI) -> None:
+        self.symbol_map = {}
+        self.engine = Quantify_systeam_online()
+        # formal正式啟動環境
+        self.dataprovider_online = DataProvider_online(formal=True)
+        self.GUI = GUI
+
+    def printfunc(self, *args):
+        self.GUI.print_info(*args)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,15 +1,49 @@
-
-# import json
-
-# with open('test.txt', 'r') as file:
-#     data = json.loads(file.read())
-
-#     out_dict = {}
-#     for symbol in data['symbols']:
-#         out_dict.update({symbol['symbol']: symbol['filters'][2]['minQty']})
-
-#     print(out_dict)
-# ETHUSDT
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import QThread
+import sys
+import time
 
 
-print(divmod(1, 2))
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('oxxo.studio')
+        self.resize(300, 200)
+        self.ui()
+        self.run()
+
+    def ui(self):
+        self.label_a = QtWidgets.QLabel(self)      # 第一個 QLabel
+        self.label_a.setGeometry(10, 10, 100, 30)
+
+        self.label_b = QtWidgets.QLabel(self)      # 第二個 QLabel
+        self.label_b.setGeometry(10, 50, 100, 30)
+
+    def a(self):
+        for i in range(0, 5):
+            self.label_a.setText(str(i))      # 每次迴圈執行時設定文字
+            print('A:', i)
+            time.sleep(0.5)              # 等待 0.5 秒
+
+    def b(self):
+        for i in range(0, 50, 10):
+            self.label_b.setText(str(i))      # 每次迴圈執行時設定文字
+            print('B:', i)
+            time.sleep(0.5)              # 等待 0.5 秒
+
+    def run(self):
+        self.thread_a = QThread()
+        self.thread_a.run = self.a
+        self.thread_a.start()
+
+        self.thread_b = QThread()
+        self.thread_b.run = self.b
+        self.thread_b.start()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    Form = MyWidget()
+    Form.show()
+    print('主視窗出現')
+    sys.exit(app.exec())
