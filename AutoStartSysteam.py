dbe933475a91684f135f61ef1d7cce9da78ab37a
lewis <lewis@mail.ybico.com.tw>
2023-05-09 10:38:11 +0800

    fix autostartsysteam

--- AutoStartSysteam.py
+++ AutoStartSysteam.py
@@ -4,8 +4,11 @@ import os
 from utils.Debug_tool import debug
 import logging
 
+python_path = r'C:\Users\user\Desktop\程式專區\TradingSysteam\Scripts\python'
+sys_path = r'C:\Users\user\Desktop\程式專區\TradingSysteam\Crypto_trading\TradeUi_TC.py'
+
 process = subprocess.Popen(
-    [r'C:\Users\user\Desktop\程式專區\TradingSysteam\Scripts\python', r'C:\Users\user\Desktop\程式專區\TradingSysteam\Crypto_trading\TradeUi_TC.py'])
+    [python_path, sys_path])
 
 while True:
     if process.poll() is None:
@@ -14,6 +17,6 @@ while True:
         debug.record_msg("System crashed, restart the program.",
                          log_level=logging.error)
         process = subprocess.Popen(
-            [r'C:\Users\user\Desktop\程式專區\TradingSysteam\Scripts\python', r'C:\Users\user\Desktop\程式專區\TradingSysteam\Crypto_trading\TradeUi_TC.py', '--autostart'])
+            [python_path, sys_path, '--autostart'])
 
     time.sleep(10)
